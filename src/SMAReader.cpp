3f4b0dafa67098fe94ee9b218b65686ad7976f54
pkoerber <7548669+pkoerber@users.noreply.github.com>
2020-10-31 18:59:08 +0100

    Initial upload code

---
+++ src/SMAReader.cpp
@@ -0,0 +1,198 @@
+#include "SMAReader.h"
+
+
+bool SMAReader::postSMA(const char* postURL, const char* postMessage, DynamicJsonDocument& jsonResult) {
+    DEBUG_SMAREADER("[HTTP] POST... URL: %s\n", postURL);
+    DEBUG_SMAREADER("[HTTP] POST... message: %s\n", postMessage);
+
+    http.useHTTP10(true);
+    http.begin(postURL);
+    http.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0");
+    http.addHeader("Content-Type", "application/json");
+    http.addHeader("Accept", "application/json, text/plain, */*");
+
+    int httpCode = http.POST(postMessage);
+    if(httpCode!=HTTP_CODE_OK) {
+        DEBUG_SMAREADER("[HTTP] POST... failed, error: %d: %s\n", httpCode, http.errorToString(httpCode).c_str());
+        http.end();
+        return false;
+    }
+
+    #if defined(DEBUG_SMAREADER_ON) && defined(DEBUG_SMAREADER_PORT)
+    ReadLoggingStream loggingStream(http.getStream(), DEBUG_SMAREADER_PORT);
+    DeserializationError error=deserializeJson(jsonResult, loggingStream);
+    #else
+    DeserializationError error=deserializeJson(jsonResult, http.getStream());
+    #endif
+    DEBUG_SMAREADER("\n");
+        
+    http.end();
+    if (error) {
+      DEBUG_SMAREADER("Parsing failed: %s\n", error.c_str());
+      return false;
+    }
+    return true;
+}
+
+bool SMAReader::authorize() {
+    char authURL[40];
+    sprintf(authURL, "http://%d.%d.%d.%d/dyn/login.json", _inverterAddress[0], _inverterAddress[1], _inverterAddress[2], _inverterAddress[3]);
+    char postText[40];
+    sprintf(postText, "{\"right\":\"%s\", \"pass\":\"%s\"}", _accountType, _passwd);
+
+    /* Stream is considered read-only input, so the strings have to be stored too, the minimum size is then 2*JSON_OBJECT_SIZE(1)+28 */
+    DynamicJsonDocument jsonResult(2*JSON_OBJECT_SIZE(1)+35);
+    bool isSuccess=postSMA(authURL, postText, jsonResult);
+    if(isSuccess) {
+        const char* res=jsonResult["result"]["sid"].as<char*>();
+        if(res==nullptr) {
+           isSuccess=false;
+           DEBUG_SMAREADER("Unexpected JSON format\n");
+        }
+        else {
+          _sid=res;
+          DEBUG_SMAREADER("Successful authorization, sid: %s\n", _sid.c_str());
+        }
+    }
+    return isSuccess;
+}
+
+bool SMAReader::logout() {
+    DEBUG_SMAREADER("Logging out\n");
+    if(_sid=="") {
+      DEBUG_SMAREADER("failed, empty sid\n");
+      return false;
+    }
+    char logoutURL[60];
+    sprintf(logoutURL, "http://%d.%d.%d.%d/dyn/logout.json?sid=%s", _inverterAddress[0], _inverterAddress[1], _inverterAddress[2], _inverterAddress[3], _sid.c_str());
+    DynamicJsonDocument jsonResult(100);
+    bool isSuccess=postSMA(logoutURL, "{}", jsonResult);
+    return isSuccess;
+}
+
+bool SMAReader::getValuesAux(int numKeys, const String* keys, DynamicJsonDocument& doc, JsonVariant& result) {
+    char scriptURL[60];
+    char postText[30+numKeys*17];
+    char keyString[numKeys*17];
+    strcpy(keyString, "\"");
+    for(int i=0;i<numKeys;i++) {
+      if(keys[i].length()>17) {
+        DEBUG_SMAREADER("Invalid key %d: %s, too long\n", i, keys[i].c_str());
+        return false;
+      }
+      if(i>0) strcat(keyString, "\",\"");
+      strcat(keyString, keys[i].c_str());
+    }
+    strcat(keyString, "\"");
+    sprintf(postText, "{\"keys\":[%s], \"destDev\":[]}", keyString);
+    
+    for(byte i=0;i<_numTries;i++) {
+      DEBUG_SMAREADER("Try: %d\n", i);
+      if(authorize()) {
+        sprintf(scriptURL, "http://%d.%d.%d.%d/dyn/getValues.json?sid=%s", _inverterAddress[0], _inverterAddress[1], _inverterAddress[2], _inverterAddress[3], _sid.c_str());
+        bool postSuccess=postSMA(scriptURL, postText, doc);
+        logout();
+        if(postSuccess) {
+           JsonObject result0=doc["result"];
+           for(JsonPair p: result0) {
+              result=p.value();
+              DEBUG_SMAREADER("Parsed JSON successfully, try: %d\n", i+1);
+              return true;
+           }
+           DEBUG_SMAREADER("Unexpected JSON format, try: %d\n", i+1);
+        }
+      }
+    }
+    return false;
+}
+
+bool SMAReader::getValues(int numKeys, const String* keys, int* values) {
+    /* Stream is considered read-only input, so the strings have to be stored too, the minimum size is then 
+    // numKeys*JSON_ARRAY_SIZE(1)+(2*numKeys+2)*JSON_OBJECT_SIZE(1)+JSON_OBJECT_SIZE(numKeys)+21+20*numkeys */
+    DynamicJsonDocument doc(numKeys*JSON_ARRAY_SIZE(1)+(2*numKeys+2)*JSON_OBJECT_SIZE(1)+JSON_OBJECT_SIZE(numKeys)+40+20*numKeys);
+    JsonVariant result;
+    if(getValuesAux(numKeys, keys, doc, result)) {
+       for(int i=0;i<numKeys;i++) {
+          JsonVariant val=result[keys[i]]["1"][0]["val"];
+          if(val.is<int>()) {
+              values[i]=val.as<int>();
+              DEBUG_SMAREADER("Int value: %d, key: %s, value: %d\n", i, keys[i].c_str(), values[i]);
+          } else {
+              values[i]=-1;
+              DEBUG_SMAREADER("Value not found or not integer: %d, key: %s\n", i, keys[i].c_str());
+          }
+       }
+       return true;
+    }
+    return false;
+}
+
+bool SMAReader::getValues(int numKeys, const String* keys, String* values) {
+    /* Stream is considered read-only input, so the strings have to be stored too, the minimum size is then 
+    // numKeys*JSON_ARRAY_SIZE(1)+(2*numKeys+2)*JSON_OBJECT_SIZE(1)+JSON_OBJECT_SIZE(numKeys)+21+20*numkeys 
+    // add extra for 20*numKeys for string results */
+    DynamicJsonDocument doc(numKeys*JSON_ARRAY_SIZE(1)+(2*numKeys+2)*JSON_OBJECT_SIZE(1)+JSON_OBJECT_SIZE(numKeys)+40+40*numKeys);
+    JsonVariant result;
+    if(getValuesAux(numKeys, keys, doc, result)) {
+       for(int i=0;i<numKeys;i++) {
+          JsonVariant val=result[keys[i]]["1"][0]["val"];
+          if(val.is<char*>()) {
+              values[i]=val.as<char*>();
+              DEBUG_SMAREADER("String value: %d, key: %s, value: %s\n", i, keys[i].c_str(), values[i].c_str());
+          } else if(val.is<int>()) {
+              values[i]=val.as<int>();
+              DEBUG_SMAREADER("Int value: %d, key: %s, value: %s\n", i, keys[i].c_str(), values[i].c_str());
+          } else {
+              values[i]="";
+              DEBUG_SMAREADER("Value not found: %d, key: %s\n", i, keys[i].c_str());
+          }
+       }
+       return true;
+    }
+    return false;
+}
+
+int SMAReader::getLog(uint32_t startTime, uint32_t endTime, uint32_t* values, uint32_t* timestamps) {
+    char scriptURL[60];
+    char postText[70];
+    sprintf(postText, "{\"key\":28672, \"destDev\":[],\"tStart\":%lu,\"tEnd\":%lu}", startTime, endTime);
+    int numValues=max((int)(endTime-startTime)/300+1,1);
+    DynamicJsonDocument doc(JSON_ARRAY_SIZE(numValues) + 2*JSON_OBJECT_SIZE(1) + numValues*JSON_OBJECT_SIZE(2)+45+4*numValues);
+    for(byte i=0;i<_numTries;i++) {
+      DEBUG_SMAREADER("Try: %d\n", i);
+      if(authorize()) {
+        sprintf(scriptURL, "http://%d.%d.%d.%d/dyn/getLogger.json?sid=%s", _inverterAddress[0], _inverterAddress[1], _inverterAddress[2], _inverterAddress[3], _sid.c_str());
+        bool postSuccess=postSMA(scriptURL, postText, doc);
+        logout();
+        if(postSuccess) {
+           JsonObject result0=doc["result"];
+           for(JsonPair p: result0) {
+              int j=0;
+              for(JsonObject obj: p.value().as<JsonArray>()) {
+                 JsonVariant timestamp=obj["t"];
+                 JsonVariant val=obj["v"];
+                 if(val.is<int>()) values[j]=val.as<uint32_t>(); else values[j]=-1;
+                 if(timestamps!=nullptr) {
+                  if(timestamp.is<int>()) timestamps[j]=timestamp.as<uint32_t>(); else timestamps[j]=-1;
+                 }
+                 j++;
+              }
+              DEBUG_SMAREADER("Parsed JSON successfully, try: %d\n", i+1);
+              return j;
+           }
+           DEBUG_SMAREADER("Unexpected JSON format, try: %d\n", i+1);
+        }
+      }
+    }
+    return -1;
+}
+
+bool SMAReader::getAllValues() {
+    if(!authorize()) return false;
+    char scriptURL[65];
+    sprintf(scriptURL, "http://%d.%d.%d.%d/dyn/getAllOnlValues.json?sid=%s", _inverterAddress[0], _inverterAddress[1], _inverterAddress[2], _inverterAddress[3], _sid.c_str());
+    char postText[]="{\"destDev\":[]}";
+    DynamicJsonDocument jsonResult(10000);
+    bool isSuccess=postSMA(scriptURL, postText, jsonResult);
+    return isSuccess;
+}
---
+++ src/SMAReader.h
@@ -0,0 +1,146 @@
+/*
+  SMAReader.h - Library for reading data from an SMA SunnyBoy Inverter.
+  Info on SMA SunnyBoy API based on https://github.com/Dymerz/SMA-SunnyBoy (Python)
+  and https://github.com/martijndierckx/sunnyboy-influxdb (Javascript)
+*/
+
+#ifndef SMAReader_h
+#define SMAReader_h
+
+//#define DEBUG_SMAREADER_ON 1
+#define DEBUG_SMAREADER_PORT Serial
+
+#include "Arduino.h"
+
+#ifdef ESP8266
+#include <ESP8266HTTPClient.h>
+#endif
+
+#ifdef ESP32
+#include <HTTPClient.h>
+#endif
+
+#include <ArduinoJson.h>
+#include <StreamUtils.h>
+
+
+#if defined(DEBUG_SMAREADER_ON) && defined(DEBUG_SMAREADER_PORT)
+#define DEBUG_SMAREADER(fmt, ...) DEBUG_SMAREADER_PORT.printf_P( (PGM_P)PSTR(fmt), ## __VA_ARGS__ )
+#endif
+
+#ifndef DEBUG_SMAREADER
+#define DEBUG_SMAREADER(...) do { (void)0; } while (0)
+#endif
+
+#define SMAREADER_USER "usr"
+#define SMAREADER_INSTALLER "istl"
+
+#define KEY_POWER "6100_40263F00" // W
+#define KEY_ENERGY_TODAY "6400_00262200" // Wh
+#define KEY_ENERGY_TOTAL "6400_00260100" // Wh
+
+/* AC values */
+#define KEY_AC_L1_POWER "6100_40464000" // W (only one if only one phase)
+#define KEY_AC_L1_VOLTAGE "6100_00464800" // 1e-2 V (only one if only one phase)
+#define KEY_AC_L1_CURRENT "6100_40465300" // mA (only one if only one phase)
+#define KEY_AC_L2_POWER "6100_40464100" // W (only if multiple phases)
+#define KEY_AC_L2_VOLTAGE "6100_00464900" // 1e-2 V (only if multiple phases)
+#define KEY_AC_L2_CURRENT "6100_40465400" // mA (only if multiple phases)
+#define KEY_AC_L3_POWER "6100_40464200" // W (only if multiple phases)
+#define KEY_AC_L3_VOLTAGE "6100_00464A00" // 1e-2 V (only if multiple phases)
+#define KEY_AC_L3_CURRENT "6100_40465500" // mA (only if multiple phases)
+#define KEY_AC_L1L2_VOLTAGE "6100_00464B00" // 1e-2 V (only if multiple phases)
+#define KEY_AC_L2L3_VOLTAGE "6100_00464C00" // 1e-2 V (only if multiple phases)
+#define KEY_AC_L3L1_VOLTAGE "6100_00464D00" // 1e-2 V (only if multiple phases)
+#define KEY_AC_FREQUENCY "6100_00465700" // 1e-2 Hz 
+
+/* DC values */
+#define KEY_DC_POWER "6380_40251E00" // W
+#define KEY_DC_VOLTAGE "6380_40451F00" // 1e-2 V
+#define KEY_DC_CURRENT "6380_40452100" // mA
+
+#define KEY_OPERATING_TIME "6400_00462E00" // s
+#define KEY_FEED_IN_TIME "6400_00462F00" // s
+
+/* Device info */
+#define KEY_ETHERNET_IP "6180_104A9A00" // String
+#define KEY_ETHERNET_DNS_IP "6180_104A9D00" // String
+#define KEY_ETHERNET_NETMASK "6180_104A9B00" // String
+#define KEY_ETHERNET_GATEWAY_IP "6180_104A9C00" // String
+
+#define KEY_WLAN_IP "6180_104AB700" // String
+#define KEY_WLAN_DNS_IP "6180_104ABA00" // String
+#define KEY_WLAN_NETMASK "6180_104AB800" // String
+#define KEY_WLAN_GATEWAY_IP "6180_104AB900" // String
+#define KEY_WLAN_STRENGTH "6100_004AB600" // percentage
+
+#define KEY_DEVICE_WARNING "6100_00411F00"
+#define KEY_DEVICE_ERROR "6100_00412000"
+#define KEY_DEVICE_OK "6100_00411E00" // Gives nominal power (W) if ok
+
+class SMAReader {
+  public:
+  
+     /* Create an SMAReader object,
+      *  inverterAddress: IP address of the inverter, e.g. 192.168.0.xxx
+      *  accountType: one of the predefined values SMAREADER_USER or SMAREADER_INSTALLER
+      *  passwd: password corresponding to the accountType (USER or INSTALLER) 
+      *  numTries: how many times to try to connect: often does not work from the first time
+      */
+     SMAReader(IPAddress inverterAddress, const char* accountType, const char* passwd, byte numTries=5) { 
+        _inverterAddress=inverterAddress; 
+        _accountType=accountType;
+        _passwd=passwd;
+        _numTries=numTries;
+     }
+
+     void setNumTries(byte numTries) { _numTries=numTries; }
+
+     /* Get the values corresponding to the keys
+      * Does not support keys with String values: they get -1
+      * keys: String array specifying which values to get, use the predefined KEY values in this header like KEY_POWER_CURRENT 
+      * values: int array to store the resulting values
+      * returns: true if success (after the specified number of tries) else false
+      * Note: if the operation succeeded, but there is an error in getting one of the keys, it gets value -1 
+      */
+     bool getValues(int numKeys, const String* keys, int* values);
+     
+     /* Get the values corresponding to the keys
+      * Same as the previous, but supports String values
+      * keys: String array specifying which values to get, use the predefined KEY values in this header like KEY_POWER_CURRENT 
+      * values: String array to store the resulting values, everything value is converted to a String
+      * returns: true if success (after the specified number of tries) else false
+      * Note: if the operation succeeded, but there is an error in getting one of the keys, it gets value "" 
+      */
+     bool getValues(int numKeys, const String* keys, String* values);
+     
+     /* Get the log of total energy production since start (in Wh) between certain times with intervals of 5 min
+      * startTime: start time of the interval as unix timestamp (seconds after 01/01/1970, midnight UTC) 
+      * endTime: end time of the interval as unix timestamp  
+      * values: uint32_t array to store the values
+      * timestamps: unint32_t array to store the timestamps of the values, if not needed supply null pointer
+      * returns: number of values if success, otherwise -1
+      */
+     int getLog(uint32_t startTime, uint32_t endTime, uint32_t* values, uint32_t* timestamps=nullptr);
+     bool getAllValues();
+     
+     
+  private:
+     IPAddress _inverterAddress;
+     const char* _passwd;
+     const char* _accountType;
+     String _sid="";
+     byte _numTries;
+
+     HTTPClient http;
+     bool getValuesAux(int numKeys, const String* keys, DynamicJsonDocument& doc, JsonVariant& result);
+     bool postSMA(const char* postURL, const char* postMessage, DynamicJsonDocument& jsonResult);
+     bool authorize();
+     bool logout();
+     
+
+     
+};
+
+
+#endif
